fa046671af7eb0dc7d2b8eef3f73f0ab473a0f3d
Evin.Leon <leon.tobar2027@hilasal.superate.org.sv>
2025-11-13 11:51:53 -0600

    pato

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import NavBar from "@/components/Navbar";
+import NavBar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <>
